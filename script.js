a0ae90fc733094297132472883806a1bf8874d94
Rebecca Ariss <rebeccamarionruth@gmail.com>
2024-04-24 16:30:56 -0400

    Remove hard-coded values for countdown, set up values for working with Date object

--- script.js
+++ script.js
@@ -2,19 +2,46 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
 let countdownTitle = '';
 let countdownDate = '';
+let countdownValue = Date;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 // Set date input minimum with today's date:
 // ISO - International Organization for Standardization (date formatting)
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
 
+// Populate countdown & complete UI:
+function updateDOM() {
+  const now = new Date().getTime();
+  const distance = countdownValue - now;
+  
+  const days = Math.floor(distance / day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minute);
+  const seconds = Math.floor((distance % minute) / second);
+  console.log(days, hours, minutes, seconds);
+};
+
 // Take values from form input:
 function updateCountdown(e) {
   e.preventDefault(); // avoids network request/page refresh on submit
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.srcElement[1].value;
+
+  // Get number version of current date, update DOM:
+  countdownValue = new Date(countdownDate).getTime();
+  updateDOM();
 };
 
 // Event Listeners
